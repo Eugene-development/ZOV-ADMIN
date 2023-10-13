5801734a4b9d1cee36c29efee8c80ac88c66f712
Eugene Che <indexpro24@gmail.com>
2023-10-13 20:12:24 +0300

    c

--- project/src/UI/Layout/Header/v1/index.jsx
+++ project/src/UI/Layout/Header/v1/index.jsx
@@ -51,17 +51,8 @@ const { user } = useAuth({ middleware: 'auth' })
         <div className="">
           <div className="sticky top-0 z-40 lg:mx-auto lg:max-w-full lg:px-8">
             <div className="flex h-16 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-0 lg:shadow-none">
-              <button
-                type="button"
-                className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-                onClick={() => setSidebarOpen(true)}
-              >
-                <span className="sr-only">Open sidebar</span>
-                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-              </button>
 
               {/* Separator */}
-              <div className="h-6 w-px bg-gray-200 lg:hidden" aria-hidden="true" />
 <button
                                 type="button"
                                 className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
@@ -84,7 +75,7 @@ const { user } = useAuth({ middleware: 'auth' })
                   <input
                     id="search-field"
                     className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                    placeholder="Search..."
+                    placeholder="Поиск..."
                     type="search"
                     name="search"
                   />
@@ -108,7 +99,7 @@ const { user } = useAuth({ middleware: 'auth' })
                         alt=""
                       />
                       <span className="hidden lg:flex lg:items-center">
-                        <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
+                        <span className="ml-4 text-base font-semibold leading-6 text-gray-900" aria-hidden="true">
                           {user?.name}
                         </span>
                         <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
